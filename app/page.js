ec97a12e3602df5f85996cee109083a64607fb01
mellykelkun <mellyjacksonarthurkevin66@gmail.com>
2026-08-21 12:09:18 +0000

    portfolio: servir le CV et la photo depuis le projet

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import CarteProjet from "../composants/CarteProjet";
 import { projets, projetsPhares } from "../donnees/projets";
@@ -15,7 +14,7 @@ export default function Accueil() {
             <div className="hero-actions">
               <Link className="bouton bouton-primaire" href="/projets">Explorer mes projets</Link>
               <Link className="bouton bouton-secondaire" href="/contact">Me contacter</Link>
-              <a className="bouton bouton-secondaire" href="/CV_Kablankan_Tiemele_Israel_Kevin.pdf" download>Télécharger mon CV ↓</a>
+              <a className="bouton bouton-secondaire" href="/cv">Télécharger mon CV ↓</a>
             </div>
           </div>
           <aside className="carte-profil">
@@ -24,13 +23,12 @@ export default function Accueil() {
               <span>Disponible pour opportunités & collaborations</span>
             </div>
             <div className="photo-profil-cadre">
-              <Image
+              <img
                 className="photo-profil"
-                src="/kablankan-tiemele-israel-kevin.jpg"
+                src="/portrait-professionnel.svg"
                 alt="Portrait professionnel de Kablankan Tiemele Israël Kévin"
-                width={900}
-                height={1125}
-                priority
+                width="300"
+                height="375"
               />
             </div>
             <h2>Kablankan Tiemele Israël Kévin</h2>
